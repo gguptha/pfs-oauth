65f59e3b34d4dc845c37f1395411a12d41d61182
sajeevkhan <sajeev.khan@gmail.com>
2019-09-16 13:12:47 +0200

    Correction by Sajeev - Unable to Authenticate Risk Portal - Temporary FIX

--- src/main/java/com/sap/pfs/oauth/auth/AuthenticationSuccessEventListener.java
+++ src/main/java/com/sap/pfs/oauth/auth/AuthenticationSuccessEventListener.java
@@ -13,11 +13,18 @@ public class AuthenticationSuccessEventListener implements ApplicationListener<A
     private LoginAttemptService loginAttemptService;
 
     public void onApplicationEvent(AuthenticationSuccessEvent event) {
-        User user = (User)event.getAuthentication().getPrincipal();
-        String email = user.getUsername();
+
+        try {
+            User user = (User) event.getAuthentication().getPrincipal();
+            String email = user.getUsername();
 //
 //        String email = (String) event.getAuthentication().getPrincipal();
 
-        loginAttemptService.loginSucceeded(email);
+            loginAttemptService.loginSucceeded(email);
+        }catch (ClassCastException ex){
+            String email = (String) event.getAuthentication().getPrincipal();
+            loginAttemptService.loginSucceeded(email);
+
+        }
     }
 }
